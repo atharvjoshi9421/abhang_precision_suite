217be423aefd27595f17a66c7f1fbf9166dcf47c
Atharv Joshi <atharvjoshi8888@gmail.com>
2026-04-13 12:43:24 +0530

    initial commit

--- asset/script_js/quote.js
+++ asset/script_js/quote.js
@@ -64,15 +64,5 @@
         $('html, body').animate({scrollTop: 0}, 600);
       });
 
-      // 4. Dark Mode / Light Mode Toggle Logic 
-      $('#themeToggle').click(function() {
-        $('body').toggleClass('dark-mode');
-        
-        // Change icon based on mode
-        if($('body').hasClass('dark-mode')) {
-          $(this).html("<i class='bx bx-sun'></i>"); // Sun icon for light mode switch
-        } else {
-          $(this).html("<i class='bx bx-moon'></i>"); // Moon icon for dark mode switch
-        }
-      });
+  
     
